ced71d35e87296fa4b1e0dbfbf981951c1b3b78e
Evan Oxfeld <eoxfeld@nearinfinity.com>
2012-08-08 16:52:36 -0400

    Add compressed zip test

--- test/unzipTest.js
+++ test/unzipTest.js
@@ -13,7 +13,8 @@ test("uncompressed archive", function (t) {
   temp.mkdir('node-unzip-', function (err, dirPath) {
     var unzipParser = unzip.Parse();
     unzipParser.on('error', function(err) {
-      t.fail(err);
+      console.log(err);
+      return t.fail(err);
     });
 
     var writer = fstream.Writer(dirPath);
@@ -21,7 +22,30 @@ test("uncompressed archive", function (t) {
       t.fail(err);
     });
     writer.on('end', function() {
-      //some code
+      console.error('end', arguments);
+      t.end();
+    });
+
+    fs.createReadStream(archive).pipe(unzipParser).pipe(writer);
+  });
+});
+
+test("compressed archive", function (t) {
+  var archive = path.join(__dirname, './data/compressed.zip');
+
+  temp.mkdir('node-unzip-', function (err, dirPath) {
+    var unzipParser = unzip.Parse();
+    unzipParser.on('error', function(err) {
+      console.log(err);
+      return t.fail(err);
+    });
+
+    var writer = fstream.Writer(dirPath);
+    writer.on('error', function(err) {
+      t.fail(err);
+    });
+    writer.on('end', function() {
+      console.error('end', arguments);
       t.end();
     });
 
