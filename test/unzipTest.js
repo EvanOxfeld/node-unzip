7e53bfaa47b32a73192e3ffa35a7db0504d7d0c1
Evan Oxfeld <eoxfeld@nearinfinity.com>
2012-08-10 23:03:53 -0400

    Refactor tests to confirm unzipped results

--- test/unzipTest.js
+++ test/unzipTest.js
@@ -5,10 +5,11 @@ var fs = require('fs');
 var path = require('path');
 var fstream = require('fstream');
 var temp = require('temp');
+var dirdiff = require('dirdiff');
 var unzip = require('../');
 
 test("uncompressed archive", function (t) {
-  var archive = path.join(__dirname, './data/uncompressed.zip');
+  var archive = path.join(__dirname, '../testData/uncompressed/archive.zip');
 
   temp.mkdir('node-unzip-', function (err, dirPath) {
     if (err) {
@@ -23,16 +24,26 @@ test("uncompressed archive", function (t) {
     writer.on('error', function(err) {
       t.fail(err);
     });
-    writer.on('close', function() {
-      t.end();
-    });
+    writer.on('close', testExtractionResults);
 
     fs.createReadStream(archive).pipe(unzipParser).pipe(writer);
+
+    function testExtractionResults() {
+      dirdiff(path.join(__dirname, '../testData/uncompressed/inflated'), dirPath, {
+        fileContents: true
+      }, function (err, diffs) {
+        if (err) {
+          return t.fail(err);
+        }
+        t.equal(0, diffs.length, 'archive directory incorrect');
+        return t.end();
+      });
+    }
   });
 });
 
 test("compressed archive", function (t) {
-  var archive = path.join(__dirname, './data/compressed.zip');
+  var archive = path.join(__dirname, '../testData/compressed/archive.zip');
 
   temp.mkdir('node-unzip-', function (err, dirPath) {
     if (err) {
@@ -47,10 +58,20 @@ test("compressed archive", function (t) {
     writer.on('error', function(err) {
       t.fail(err);
     });
-    writer.on('close', function() {
-      t.end();
-    });
+    writer.on('close', testExtractionResults);
 
     fs.createReadStream(archive).pipe(unzipParser).pipe(writer);
+
+    function testExtractionResults() {
+      dirdiff(path.join(__dirname, '../testData/compressed/inflated'), dirPath, {
+        fileContents: true
+      }, function (err, diffs) {
+        if (err) {
+          return t.fail(err);
+        }
+        t.equal(0, diffs.length, 'archive directory incorrect');
+        return t.end();
+      });
+    }
   });
 });
