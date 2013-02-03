86ea91fbf96edfe548fabbea0d784297bb12e7ed
Evan Oxfeld <eoxfeld@nearinfinity.com>
2013-02-03 14:07:19 -0500

    Add test for OS X Finder zips

--- test/unzipTest.js
+++ test/unzipTest.js
@@ -37,8 +37,8 @@ test("uncompressed archive", function (t) {
   });
 });
 
-test("compressed archive", function (t) {
-  var archive = path.join(__dirname, '../testData/compressed/archive.zip');
+test("compressed archive w/ file sizes known prior to zlib inflation (created by POSIX zip)", function (t) {
+  var archive = path.join(__dirname, '../testData/compressed-standard/archive.zip');
 
   temp.mkdir('node-unzip-', function (err, dirPath) {
     if (err) {
@@ -53,7 +53,36 @@ test("compressed archive", function (t) {
     fs.createReadStream(archive).pipe(unzipExtractor);
 
     function testExtractionResults() {
-      dirdiff(path.join(__dirname, '../testData/compressed/inflated'), dirPath, {
+      dirdiff(path.join(__dirname, '../testData/compressed-standard/inflated'), dirPath, {
+        fileContents: true
+      }, function (err, diffs) {
+        if (err) {
+          throw err;
+        }
+        t.equal(diffs.length, 0, 'extracted directory contents');
+        t.end();
+      });
+    }
+  });
+});
+
+test("compressed archive w/ file sizes unknown prior to zlib inflation (created by OS X Finder)", function (t) {
+  var archive = path.join(__dirname, '../testData/compressed-OSX-Finder/archive.zip');
+
+  temp.mkdir('node-unzip-', function (err, dirPath) {
+    if (err) {
+      throw err;
+    }
+    var unzipExtractor = unzip.Extract({ path: dirPath });
+    unzipExtractor.on('error', function(err) {
+      throw err;
+    });
+    unzipExtractor.on('close', testExtractionResults);
+
+    fs.createReadStream(archive).pipe(unzipExtractor);
+
+    function testExtractionResults() {
+      dirdiff(path.join(__dirname, '../testData/compressed-OSX-Finder/inflated'), dirPath, {
         fileContents: true
       }, function (err, diffs) {
         if (err) {
@@ -67,7 +96,7 @@ test("compressed archive", function (t) {
 });
 
 test("pipe a single file entry out of a zip", function (t) {
-  var archive = path.join(__dirname, '../testData/compressed/archive.zip');
+  var archive = path.join(__dirname, '../testData/compressed-standard/archive.zip');
 
   fs.createReadStream(archive)
     .pipe(unzip.Parse())
@@ -76,7 +105,7 @@ test("pipe a single file entry out of a zip", function (t) {
         var writableStream = new streamBuffers.WritableStreamBuffer();
         writableStream.on('close', function () {
           var str = writableStream.getContentsAsString('utf8');
-          var fileStr = fs.readFileSync(path.join(__dirname, '../testData/compressed/inflated/file.txt'), 'utf8')
+          var fileStr = fs.readFileSync(path.join(__dirname, '../testData/compressed-standard/inflated/file.txt'), 'utf8')
           t.equal(str, fileStr);
           t.end();
         });
