aac43bbe02f25a4a3a2ed01faf7bca36a85c8ada
Rich Hodgkins <rhodgkins@gmail.com>
2018-08-24 09:29:17 +0100

    Failed tests for uncaught FILE_ENDED error

---
+++ test/notArchive.js
@@ -0,0 +1,47 @@
+'use strict'
+
+var test = require('tap').test;
+var fs = require('fs');
+var path = require('path');
+var temp = require('temp');
+var unzip = require('../');
+
+var archive = path.join(__dirname, '../package.json');
+
+test('parse a file that is not an archive', function (t) {
+
+  var unzipParser = unzip.Parse();
+  fs.createReadStream(archive).pipe(unzipParser);
+  unzipParser.on('error', err => {
+    t.ok(err.message.indexOf('invalid signature: 0x') !== -1);
+    t.end();
+  });
+
+  unzipParser.on('close', () => t.fail('Archive was parsed', d))
+});
+
+test('extract a file that is not an archive', function (t) {
+
+  temp.mkdir('node-unzip-', (err, dirPath) => {
+    if (err) {
+      throw err;
+    }
+    var unzipExtractor = unzip.Extract({ path: dirPath });
+    unzipExtractor.on('error', err => {
+      t.ok(err.message.indexOf('invalid signature: 0x') !== -1);
+      t.end();
+    });
+    unzipExtractor.on('close', () => t.fail('Archive was extracted'));
+
+    fs.createReadStream(archive).pipe(unzipExtractor);
+  });
+});
+
+test('get content of a single file entry out of a file that is not an archive', function (t) {
+  unzip.Open.file(archive)
+    .then(d => t.fail('Archive was opened', d))
+    .catch(err => {
+      t.equal(err.message, 'FILE_ENDED');
+      t.end();
+    });
+});
