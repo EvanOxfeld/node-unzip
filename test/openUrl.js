5620ccdd0f482d7ea40154f84d03d090bbf95a74
John Malkovich <john>
2018-12-11 17:38:11 +0100

    Replaced test zip archive for openUrl test

--- test/openUrl.js
+++ test/openUrl.js
@@ -7,15 +7,15 @@ var unzip = require('../');
 var request = require('request');
 
 test("get content of a single file entry out of a 502 MB zip from web", function (t) {
-  return unzip.Open.url(request,'http://www2.census.gov/geo/tiger/TIGER2015/ZCTA5/tl_2015_us_zcta510.zip')
+  return unzip.Open.url(request,'https://github.com/twbs/bootstrap/releases/download/v4.0.0/bootstrap-4.0.0-dist.zip')
     .then(function(d) {
       var file = d.files.filter(function(d) {
-        return d.path === 'tl_2015_us_zcta510.shp.iso.xml';
+        return d.path === 'css/bootstrap-reboot.min.css';
       })[0];
       return file.buffer();
     })
     .then(function(str) {
-       var fileStr = fs.readFileSync(path.join(__dirname, '../testData/tl_2015_us_zcta510.shp.iso.xml'), 'utf8');
+       var fileStr = fs.readFileSync(path.join(__dirname, '../testData/bootstrap-reboot.min.css'), 'utf8');
       t.equal(str.toString(), fileStr);
       t.end();
     });
