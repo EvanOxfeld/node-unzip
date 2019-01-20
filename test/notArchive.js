7babcca5dda8335d4c86d0acb07d95b2023b95e2
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2019-01-20 09:15:56 -0500

    Remove ES6 from notArchive test for backwards compatibility

--- test/notArchive.js
+++ test/notArchive.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var test = require('tap').test;
 var fs = require('fs');
@@ -12,26 +12,30 @@ test('parse a file that is not an archive', function (t) {
 
   var unzipParser = unzip.Parse();
   fs.createReadStream(archive).pipe(unzipParser);
-  unzipParser.on('error', err => {
+  unzipParser.on('error', function(err) {
     t.ok(err.message.indexOf('invalid signature: 0x') !== -1);
     t.end();
   });
 
-  unzipParser.on('close', () => t.fail('Archive was parsed', d))
+  unzipParser.on('close', function(d) {
+    t.fail('Archive was parsed', d);
+  });
 });
 
 test('extract a file that is not an archive', function (t) {
 
-  temp.mkdir('node-unzip-', (err, dirPath) => {
+  temp.mkdir('node-unzip-', function(err, dirPath) {
     if (err) {
       throw err;
     }
     var unzipExtractor = unzip.Extract({ path: dirPath });
-    unzipExtractor.on('error', err => {
+    unzipExtractor.on('error', function(err) {
       t.ok(err.message.indexOf('invalid signature: 0x') !== -1);
       t.end();
     });
-    unzipExtractor.on('close', () => t.fail('Archive was extracted'));
+    unzipExtractor.on('close', function() {
+      t.fail('Archive was extracted');
+    });
 
     fs.createReadStream(archive).pipe(unzipExtractor);
   });
@@ -39,8 +43,10 @@ test('extract a file that is not an archive', function (t) {
 
 test('get content of a single file entry out of a file that is not an archive', function (t) {
   unzip.Open.file(archive)
-    .then(d => t.fail('Archive was opened', d))
-    .catch(err => {
+    .then(function(d) {
+      t.fail('Archive was opened', d);
+    })
+    .catch(function(err) {
       t.equal(err.message, 'FILE_ENDED');
       t.end();
     });
