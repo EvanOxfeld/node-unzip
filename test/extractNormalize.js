b17543fc65df101da4d879c7fe2e9d521cafb6a3
huikaihoo <huikaihoo@users.noreply.github.com>
2020-04-20 20:42:26 +0800

    Fixed extract when opts.path is '.' (dot) (#159)
    * Resolve destination path to absolute path
    
    Fixed when opts.path is '.' (dot), zip slip checking will be always failed when extracting zip file
    
    * Add test for fix

--- test/extractNormalize.js
+++ test/extractNormalize.js
@@ -51,3 +51,42 @@ test("Extract should normalize the path option", function (t) {
     fs.createReadStream(archive).pipe(unzipExtractor);
   });
 });
+
+test("Extract should resolve after normalize the path option", function (t) {
+  var archive = path.join(__dirname, '../testData/compressed-standard/archive.zip');
+
+  temp.mkdir('node-unzip-normalize-2-', function (err, dirPath) {
+    if (err) {
+      throw err;
+    }
+
+    var filesDone = 0;
+
+    function getWriter() {
+      var delayStream = new Stream.Transform();
+
+      delayStream._transform = function(d, e, cb) {
+        setTimeout(cb, 500);
+      };
+
+      delayStream._flush = function(cb) {
+        filesDone += 1;
+        cb();
+        delayStream.emit('close');
+      };
+
+      return delayStream;
+    }
+
+    var unzipExtractor = unzip.Extract({ getWriter: getWriter, path: '.' });
+    unzipExtractor.on('error', function(err) {
+      throw err;
+    });
+    unzipExtractor.on('close', function() {
+      t.same(filesDone,2);
+      t.end();
+    });
+
+    fs.createReadStream(archive).pipe(unzipExtractor);
+  });
+});
