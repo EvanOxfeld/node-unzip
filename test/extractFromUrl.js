37c83b7a2b83747f70e286c5994b87e87c9bf806
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2020-06-22 10:50:17 -0400

    hotfix: remove ES6

--- test/extractFromUrl.js
+++ test/extractFromUrl.js
@@ -1,21 +1,21 @@
 "use strict";
 
-const test = require("tap").test;
-const fs = require("fs");
-const unzip = require("../");
-const os = require("os");
-const request = require("request");
+var test = require("tap").test;
+var fs = require("fs");
+var unzip = require("../");
+var os = require("os");
+var request = require("request");
 
 test("extract zip from url", function (t) {
-  const extractPath = os.tmpdir() + "/node-unzip-extract-fromURL"; // Not using path resolve, cause it should be resolved in extract() function
+  var extractPath = os.tmpdir() + "/node-unzip-extract-fromURL"; // Not using path resolve, cause it should be resolved in extract() function
   unzip.Open.url(
     request,
     "https://github.com/h5bp/html5-boilerplate/releases/download/v7.3.0/html5-boilerplate_v7.3.0.zip"
   )
-    .then((d) => d.extract({ path: extractPath }))
-    .then((d) => {
-      const dirFiles = fs.readdirSync(extractPath);
-      const isPassing =
+    .then(function(d) { return d.extract({ path: extractPath }); })
+    .then(function(d) {
+      var dirFiles = fs.readdirSync(extractPath);
+      var isPassing =
         dirFiles.length > 10 &&
         dirFiles.indexOf("css") > -1 &&
         dirFiles.indexOf("index.html") > -1 &&
